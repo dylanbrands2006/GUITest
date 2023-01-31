91e399c9718c5840cd671597a3c1c9b755d6cc64
dylanbrands2006 <dylanbrands06@gmail.com>
2023-01-31 16:41:54 +0100

    Test

---
+++ src/main/java/me/spoofy/guitest/GUITest.java
@@ -0,0 +1,17 @@
+package me.spoofy.guitest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GUITest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
